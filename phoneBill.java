df673b68af9b5954c492c5585218763f54ed157d
Costamarmar <Cmarmaras1@gmail.com>
2024-02-07 11:13:38 -0500

    opening comments

--- phoneBill.java
+++ phoneBill.java
@@ -1,7 +1,13 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
+// Programmers:  Costa Marmaras
+// Course:  CS 212
+// Due Date: 2/9/24
+// In class assesment: Phone Bill
+// Problem Statement: Create a program that will calculate a users phone bill based on their package
+//                      and data used
+// Data In: Package and how many GBs of data used
+// Data Out: Final cost taking into account the amount of GBs of data used
+// Credits: [Is your code based on an example in the book, in class, or something else?
+//            Reminder: you should never take code from the Internet or another person
 
 class HelloWorld {
     public static void main(String[] args) {
