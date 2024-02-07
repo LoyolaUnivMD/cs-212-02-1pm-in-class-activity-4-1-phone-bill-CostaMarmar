55da62f0a1d1e79792543a841ea1db1bd0e3c022
Costamarmar <Cmarmaras1@gmail.com>
2024-02-07 11:46:31 -0500

    Code but I don't think I added calculations

--- phoneBill.java
+++ phoneBill.java
@@ -9,10 +9,57 @@
 // Credits: [Is your code based on an example in the book, in class, or something else?
 //            Reminder: you should never take code from the Internet or another person
 
+//Import scanner and decimal format
+import java.util.Scanner;
+import java.text.DecimalFormat;
 class HelloWorld {
     public static void main(String[] args) {
+        //initialize scanner and decimal format, and intro print statement
+        Scanner input = new Scanner(System.in);
+        System.out.println("This program will calculate your phone bill based on your package");
+        DecimalFormat twoDigits = new DecimalFormat("$.00");
 
-        System.out.println("Phone Bill Activity!");
-        
+        //initialize the needed boolean variables
+        boolean isOverData = false;
+        boolean hasCoupon = false;
+
+        //ask the user what package they have and strips and sets it to lower case
+        System.out.println("What Phone Package Do You Have? (Green, Blue, or Purple)");
+        String phonePack = input.nextLine().strip().toLowerCase();
+
+        //keep asking the user for what package they use until they input a valid answer
+        while(!phonePack.equals("green") && !phonePack.equals("blue") && !phonePack.equals("purple"))
+        {
+            System.out.println("That is not a valid input.\nPlease input a valid input (green, blue, or purple");
+            phonePack = phonePack = input.nextLine().strip().toLowerCase();
+        }
+
+        //Ask the user if they have a coupon if they have the green package
+        if(phonePack.equals("green"))
+        {
+            System.out.println("Do you have a coupon? (Y or N)");
+            String coup = input.nextLine().strip().toUpperCase();
+            //Ask until they input a valid answer
+            while(!coup.equals("Y") && !coup.equals("N"))
+            {
+                System.out.println("That is not a valid input\nPlease input a valid input (Y/N)");
+                coup = input.nextLine().strip().toUpperCase();
+            }
+        }
+        /*if elif ternary operator to find the max GBs of data used (if its purple it is defaulted to 0
+                because data is not accounted in purple and therefore could be any number
+         */
+        int maxGB = (phonePack.equals("green"))? 2 : phonePack.equals("blue")? 4 : 0;
+
+        //Ask the user how many GBs of data they used and make sure that it is above 0
+        System.out.println("How many GBs of data did you use this month?");
+        int gbUsed = input.nextInt();
+        while(gbUsed < 0)
+        {
+            System.out.println("That is not a valid input\nPlease input a number 0 or above");
+            gbUsed = input.nextInt();
+        }
+
+
     }
 }
